3d3b8cac8813cc35376c6f05af1661a69ad7ec7b
1daw12 <1daw12@INFORMATICA-112.DOMDPTOINFJMPP.INF>
2022-03-22 16:55:12 +0100

    prueba tres

---
+++ src/main/java/com/mycompany/actividad5_ets/Prueba.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.mycompany.actividad5_ets;
+
+/**
+ * @date 22 mar. 2022
+ * @autor Carmen Nieves Hernández González
+ * @version 1.0
+ * Nombre de la Clase : Prueba
+ */
+public class Prueba {
+    public static void mostrarDatos(){
+        System.out.println("prueba uno");
+    }
+}
